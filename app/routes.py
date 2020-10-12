12b1ce732cf55c1116a020cc381053d6309eacfd
AJH <epicronster@gmail.com>
2020-10-13 00:20:00 +0200

    added registration and login templates and routes

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user
+from flask_login import login_user
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
+from flask.helpers import url_for
 
 
 
@@ -59,5 +61,26 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('index"'))
 
+
+from app import db
+from app.forms import RegistrationForm
+
+# ...
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+    
